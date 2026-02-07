c2da4b6dcd232ad3b4e17b4109d7acd8fc26f49a
moloch-- <875022+moloch--@users.noreply.github.com>
2026-02-07 09:13:20 -0800

    Add release workflow

--- cli/main.go
+++ cli/main.go
@@ -1,51 +1,68 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/sliverarmory/malasada"
+	"github.com/spf13/cobra"
 )
 
 func main() {
+	cmd := newRootCmd()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func newRootCmd() *cobra.Command {
 	var (
 		outPath    string
 		exportName string
 		compress   bool
 	)
 
-	flag.StringVar(&outPath, "o", "", "Output .bin path (default: <input>.bin)")
-	flag.StringVar(&exportName, "call-export", malasada.DefaultCallExport, "Exported function name to call after the .so is loaded")
-	flag.BoolVar(&compress, "compress", false, "Compress the embedded .so payload with aPLib (default: false)")
-	flag.BoolVar(&compress, "compression", false, "Alias for -compress")
-	flag.Parse()
+	cmd := &cobra.Command{
+		Use:   "malasada [flags] <input.so>",
+		Short: "Convert a Linux ELF shared object (.so) into an executable PIC .bin blob",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("usage: %s [flags] <input.so>", cmd.CommandPath())
+			}
+			return nil
+		},
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			soPath := args[0]
+			if outPath == "" {
+				outPath = soPath + ".bin"
+			}
 
-	if flag.NArg() != 1 {
-		_, _ = fmt.Fprintf(os.Stderr, "usage: malasada [flags] <input.so>\n")
-		os.Exit(1)
-	}
-	soPath := flag.Arg(0)
-	if outPath == "" {
-		outPath = soPath + ".bin"
+			bin, err := malasada.ConvertSharedObject(soPath, exportName, compress)
+			if err != nil {
+				return err
+			}
+
+			dir := filepath.Dir(outPath)
+			if dir != "." {
+				if err := os.MkdirAll(dir, 0o755); err != nil {
+					return fmt.Errorf("mkdir: %w", err)
+				}
+			}
+			if err := os.WriteFile(outPath, bin, 0o755); err != nil {
+				return fmt.Errorf("write %s: %w", outPath, err)
+			}
+			return nil
+		},
 	}
 
-	bin, err := malasada.ConvertSharedObject(soPath, exportName, compress)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
+	cmd.Flags().StringVarP(&outPath, "o", "o", "", "Output .bin path (default: <input>.bin)")
+	cmd.Flags().StringVar(&exportName, "call-export", malasada.DefaultCallExport, "Exported function name to call after the .so is loaded")
+	cmd.Flags().BoolVar(&compress, "compress", false, "Compress the embedded .so payload with aPLib (default: false)")
+	cmd.Flags().BoolVar(&compress, "compression", false, "Alias for --compress")
 
-	dir := filepath.Dir(outPath)
-	if dir != "." {
-		if err := os.MkdirAll(dir, 0o755); err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "mkdir: %v\n", err)
-			os.Exit(1)
-		}
-	}
-	if err := os.WriteFile(outPath, bin, 0o755); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "write %s: %v\n", outPath, err)
-		os.Exit(1)
-	}
+	return cmd
 }
